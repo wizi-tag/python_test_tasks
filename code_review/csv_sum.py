1a61465ee322efc808b7114bddaa6ea79ae47cc8
wizi-tag <a.kapyash@gmail.com>
2022-08-11 19:34:27 +0800

    completed tasks

--- code_review/csv_sum.py
+++ code_review/csv_sum.py
@@ -1,4 +1,46 @@
 import pandas as pd
+import os.path
 
-df = pd.read_csv("data.csv", sep=",", header=None)
-print(df.values.sum()==10)
+"""
+I would put the variables in the config
+or pass the file name through sys.argv,
+but the script is small and we can leave it here
+"""
+filename = 'data.csv'
+target = 10
+
+"""
+What if we have not .csv file in data.tgz?
+"""
+if not os.path.exists(filename):
+    print("[ERROR]\tFile not exists")
+else:
+    """
+    Idk why pandas is used here
+    if the data.csv format is the same as in example
+    I would use split
+
+    ```
+        with open("filename","r") as file:
+            str = file.readline()
+            sum = sum(str.split(','))
+    ```
+
+    """
+    # Change name of variable and add ".values" to get values initially
+    csv_values = pd.read_csv(filename, sep=",", header=None).values
+
+    """
+    If there are strings in data.csv or something like that,
+    then an exception will be thrown
+    """
+    try:
+        print(csv_values.sum() == target)
+    except TypeError as err:
+        print("[ERROR]\tInvalid data in "+filename)
+        exit()
+
+"""
+Also I think it would be better to pack this into a function
+So that this code can be included as a module in other places
+"""
--- tessellation/solution.py
+++ tessellation/solution.py
@@ -1,4 +1,88 @@
 
 # %%
-def main(desk_size: list[int, int]) -> list[list[int, int]]:
-    return [[0,0],[1,2], [0,0]]
+import random
+
+def get_variants(pos):
+    variants = list(map(lambda step:(step[0]+pos[0], step[1]+pos[1]), steps))
+    variants = list(filter(
+    lambda variant:
+    variant[0] >= 0 and
+    variant[0] < N and
+    variant[1] >= 0 and
+    variant[1] < M,
+    variants))
+
+    return(variants)
+
+
+def remove_visited(variants):
+    not_visited = list(filter(lambda variant:
+        desk[variant[0]][variant[1]] == None,
+        variants))
+
+    return not_visited
+
+
+def move(start_pos):
+    variants = get_variants(start_pos)
+    not_visited = remove_visited(variants)
+
+    if (not_visited):
+        next_variants = list(map(get_variants, not_visited))
+        count_list = []
+        for variant in next_variants:
+            count_list.append(len(remove_visited(variant)))
+
+        pos = not_visited[count_list.index(min(count_list))]
+
+    else:
+        pos = random.choice(variants)
+
+    return pos
+
+
+def main(desk_size):
+    pos = (0,0)
+
+    global N
+    N = desk_size[0]
+
+    global M
+    M = desk_size[1]
+
+    global desk
+    desk = [[ None for j in range(M)] for i in range(N)]
+
+    global steps
+    steps = [
+        (+2, +1),
+        (+2, -1),
+        (+1, +2),
+        (+1, -2),
+        (-2, +1),
+        (-2, -1),
+        (-1, +2),
+        (-1, -2)
+        ]
+
+    flag = True
+    step = 1
+
+    desk[pos[0]][pos[1]] = step
+    way = [list(pos)]
+
+    while flag:
+        pos = move(pos)
+        step += 1
+        desk[pos[0]][pos[1]] = step
+        way.append(list(pos))
+
+        if (step >= N*M):
+            for row in desk:
+                if None in row:
+                    flag = True
+                    break
+                else:
+                    flag = False
+
+    return way
